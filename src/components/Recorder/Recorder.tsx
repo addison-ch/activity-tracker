ef0c081590a66a01b3a195f21dcd956b1d9a47ee
Addison <74084822+addison-ch@users.noreply.github.com>
2022-01-06 19:33:17 -0500

    add time recorder component and reducer

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Recorder.css'
 
 const Recorder = () => {
     return (
-        <div>
-            
+        <div className="recorder">
+            <button className="recorder-record">
+                <span></span>
+            </button>
+            <div className="recorder-counter">00:00:00</div>
+
         </div>
     )
 }
